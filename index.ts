eedef576c9eb2f0d20abbaac8aa58377ea0a5ff0
loginovw8 <loginovw8@gmail.com>
2023-07-22 08:46:42 +0300

    Template modified.

--- index.ts
+++ index.ts
@@ -1,22 +1,21 @@
+import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express } from 'express';
-import { registerRoutes } from './routes/index';
+import { registerRoutes } from './routes/web';
 import { registerApiRoutes } from './routes/api';
 
 dotenv.config();
 
 const app: Express = express();
 
-app.use(cors({
-    origin: process.env.CORS?.split(","),
-}));
-
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set('views', path.join(__dirname.replace('/dist', ''), 'resources/views'));
 
 app.use(express.json());
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: process.env.CORS?.split(","), }));
 
 app.listen(process.env.PORT, () => {
     console.log(`App listening on port ${process.env.PORT}`);
